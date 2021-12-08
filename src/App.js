492230db2e05bf9075065bbc68a0b00b5695b8de
Jesse <jessemckinney513@gmail.com>
2021-12-08 13:10:18 -0500

    need functioning still, moving on for now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import "./App.css";
 
 function App() {
+  // let list = document.querySelectorAll(".list");
+  // for (let i = 0; i < list.length; i++) {
+  //   list[i].onmouseover = function () {
+  //     let j = 0;
+  //     while (j < list.length) {
+  //       list[j++].className = "list";
+  //     }
+  //     list[i].className = "list active";
+  //   };
+  // }
+
+  // list.forEach((elements) => {
+  //   elements.addEventListener("mouseenter", function (event) {
+  //     let bg = document.querySelector("body");
+  //     let color = event.target.getAttribute("data-color");
+  //     bg.style.backgroundColor = color;
+  //   });
+  // });
+
+  // const list = document.querySelectorAll(".list");
+  // function activeLink() {
+  //   list.forEach((item) => item.classList.remove("active"));
+  //   this.classList.add("active");
+  // }
+
+  // list.forEach((item) => item.addEventListener("click", activeLink));
+
   return (
     <>
       <div className="App">
         <div className="navigation">
           <ul>
-            <li className="list active">
+            <li className="list">
               <a href="/">
                 <span className="icon">
                   <ion-icon name="home"></ion-icon>
